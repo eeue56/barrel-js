c760159bab3f36ede7138748dda68d8a7ec9ca01
eeue56 <eeue56@users.noreply.github.com>
2015-03-17 02:00:14 +0000

    Reloading from localStorage works now

--- barrel.js
+++ barrel.js
@@ -24,20 +24,24 @@ angular.module('barrel', [])
   this.init();
 
   this._store = function() {
-    localStorage.setItem(BarrelKey, JSON.stringify(this.store));
+    localStorage.setItem(BarrelKey, angular.toJson(this.store));
   };
 
   this.save = function(id, item) {
     this.store[id] = item;
     this._store();
-  };    
+  };
+
+  this.get = function(id) {
+    return this.store[id] || null;
+  }    
 })
 
 
 .service('Barrel', function(BarrelObject) {
     
   this.child = function(id) {
-    return BarrelObject(id);
+    return new BarrelObject(id);
   };
 
 })
@@ -45,6 +49,7 @@ angular.module('barrel', [])
 
 .factory('BarrelObject', function(BarrelUtils) {
   function BarrelObject(id) {
+
     if(!(this instanceof BarrelObject)) {
       return new BarrelObject(id);
     }
@@ -52,18 +57,26 @@ angular.module('barrel', [])
     this.$id = id;
 
     this._innerArray = [];
+
+    var me = BarrelUtils.get(id); 
+
+    if (me !== null){
+      this._innerArray = me._innerArray;      
+    }
   };
 
   BarrelObject.prototype = {
     $update: function(value) {
       this._innerArray = value;
-      this.$save();
     },
     $save: function() {
       BarrelUtils.save(this.$id, this);
     },
     $push: function(object) {
       this._innerArray.push(object);
+    },
+    $asArray: function() {
+      return this._innerArray;
     }
   };
 
